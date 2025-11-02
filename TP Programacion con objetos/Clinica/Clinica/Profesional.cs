db0e062c4e40b99632b071b51c19500e267f2a2c
jimenaianina <74919441+jimenaianina@users.noreply.github.com>
2025-11-02 20:13:39 -0300

    Update Profesional.cs

--- TP Programacion con objetos/Clinica/Clinica/Profesional.cs
+++ TP Programacion con objetos/Clinica/Clinica/Profesional.cs
@@ -1,11 +1,3 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Sarabe89
- * Date: 18/10/2025
- * Time: 20:13
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
 using System.Collections.Generic;
 
