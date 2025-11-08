682859f14059965cd4d24227422ad5f2870b848b
Héctor Fabián Villavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2025-11-08 20:35:50 -0300

    Hice unos par de cambios en area y agregue los metodos

--- TP Programacion con objetos/Clinica/Clinica/Area.cs
+++ TP Programacion con objetos/Clinica/Clinica/Area.cs
@@ -13,17 +13,54 @@ namespace Clinica
 		public string nombre;
 		public List<Profesional> profesionales;
 		
-		public Area()
+		public Area(int id, string nombre, List<Profesional> profesionales)
 		{
+			this.id = id;
+			this.nombre = nombre;
+			this.profesionales = profesionales;
 		}
 		
 		public Area(int id, string nombre)
 		{
 			this.id = id;
 			this.nombre = nombre;
-			this.profesionales = new List<Profesional>();
+			profesionales = new List<Profesional>();
 		}
-		
-		
+
+		public void AgregarProfesionalArea(Profesional profesional){
+			profesionales.add(profesiona);
+		}
+
+		public void EliminarProfesional(Profesional profesional){
+			profesionales.delete(profesional);
+		}
+
+		public void MostrarProfesionales(){
+			try{
+				if (profesionales == null) {
+					throw new Exception();
+					//trow new NoHayProfesionalesExeption(); lo dejo asi para hacerlo mas adelante
+				}
+				
+				Console.WriteLine("Informacion De los profesionales");
+
+				foreach(Profesional profesional in profesionales){
+					Console.WriteLine("ID: {0}", id);
+					Console.WriteLine("Nombre:  {0}", nombre);
+					Console.WriteLine("Area: {0}", area);
+					Console.WriteLine("");
+				};
+
+            }
+            catch (Exception n)
+            {
+				Console.WriteLine(n.Message);
+            }
+		}
 	}
+
+
+		
+		
 }
+
