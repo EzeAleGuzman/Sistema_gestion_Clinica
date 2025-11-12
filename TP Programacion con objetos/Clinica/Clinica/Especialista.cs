22eb940539029963fe93475e40ccd737ec418b4d
jimenaianina <74919441+jimenaianina@users.noreply.github.com>
2025-11-12 08:42:44 -0300

    Update Especialista.cs
    Actualización de clases en base al UML

--- TP Programacion con objetos/Clinica/Clinica/Especialista.cs
+++ TP Programacion con objetos/Clinica/Clinica/Especialista.cs
@@ -13,14 +13,28 @@ namespace Clinica
 	/// <summary>
 	/// Description of Especialista.
 	/// </summary>
-	public class Especialista
+	public class Especialista:Profesional
 	{
 		
 		public double honorarios = 8500;
 		public int maxPacientesDia = 6;
 		
-		public Especialista()
+		public Especialista(int id, string nombre, double honorarios, int maxPacientesDia):base(id, nombre)
 		{
+			this.honorarios = honorarios;
+			this.maxPacientesDia = maxPacientesDia;
 		}
+
+		public override void AtenderPacientes(Consulta consulta){
+			if (listadoConsultasPendientes.count < 7) {
+				listadoConsultasPendientes.Add(consulta)
+			}
+			else {
+				Console.WriteLine("Se ha alcanzado el máximo de pacientes a atender por parte de este profesional");
+			}
+
+		public override double Calcularcosto() {
+			return listadoConsultasPendientes.count * honorarios;
+		}	
 	}
 }
