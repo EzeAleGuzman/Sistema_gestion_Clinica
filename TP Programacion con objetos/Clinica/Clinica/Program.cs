55a6cc34ba2020872bd0d0131effd7f356967b58
jimenaianina <74919441+jimenaianina@users.noreply.github.com>
2025-11-15 14:00:03 -0300

    Empecé a hacer el menú con sus opciones

--- TP Programacion con objetos/Clinica/Clinica/Program.cs
+++ TP Programacion con objetos/Clinica/Clinica/Program.cs
@@ -12,6 +12,37 @@ namespace Clinica
 {
 	class Program
 	{
+
+		public static void opciones_menu()
+		{
+			Console.WriteLine("Por favor, ingrese el número de la opción correspondiente para continuar");
+			Console.WriteLine("1. Agregar nuevo paciente");
+			Console.WriteLine("2. Buscar paciente");
+			Console.WriteLine("3. Mostrar el historial del paciente");
+			Console.WriteLine("4. Crear un nuevo médico clínico");
+			Console.WriteLine("5. Crear un nuevo médico emergentólogo");
+			Console.WriteLine("6. Crear un nuevo especialista");
+			Console.WriteLine("7. Simular día de trabajo");
+			Console.WriteLine("8. Salir del sistema");
+		}
+
+		public static void opcion1()
+		{
+			string nombre;
+			int dni;
+			int edad;
+			string coberturaSalud;
+			Console.WriteLine("Por favor, ingrese el nombre del paciente");
+			nombre = Console.ReadLine();
+			Console.WriteLine("Por favor, ingrese el dni del paciente");
+			dni = Convert.toInt32(Console.ReadLine());
+			Console.WriteLine("Por favor, ingrese la edad del paciente");
+			edad = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Por favor, ingrese la cobertura de salud del paciente");
+			coberturaSalud = Console.ReadLine();
+			paciente p1 = new Paciente(nombre, dni, edad, coberturaSalud);
+		}
+		
 		public static void Main(string[] args)
 		{
 			
@@ -19,7 +50,32 @@ namespace Clinica
 			Paciente p1 = new Paciente("Guzman Ezequiel", 34931575, 26,"IOMA",archivos);
 			Clinica cli = new Clinica(archivos);
 			cli.MostrarProfesionales(cli.profesionales);
-			
+
+			while (opcion_seleccionada != 8)
+			{
+				opciones_menu();
+				opcion_seleccionada = Convert.ToInt32(Console.ReadLine());
+				switch(opcion_seleccionada)
+				{
+					case 1: opcion1();
+					break;
+					case 2: opcion2();
+					break;
+					case 3: opcion3();
+					break;
+					case 4: opcion4();
+					break;
+					case 5: opcion5();
+					break;
+					case 6: opcion6();
+					break;
+					case 7: opcion7();
+					break;
+					default: Console.WriteLine("No es una opción válida");
+					break;
+				}
+			}
+
 			
 			// TODO: Implement Functionality Here
 			
